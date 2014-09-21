e7a1a154447cb3a0133be5f442639bf91a5b9673
Slim Amamou <slim.amamou@gmail.com>
2014-09-21 16:37:04 +0100

    premiere

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>VOX - بيع صوتك</title>
+  <link href="http://getbootstrap.com/2.3.2/assets/css/bootstrap.css" rel="stylesheet">
+<style>
+@font-face {
+  font-family:'Scheherazade';
+  src:url('http://fkr.pirate.tn/ScheherazadeRegOT.ttf');
+}
+@font-face {
+  font-family:'Droid';
+  src:url('http://fkr.pirate.tn/DroidKufi-Regular.ttf');
+}
+body { font-family: Droid; text-align: center}
+a.btn {font-family: Droid;}
+.footer { color:#fff100; background-color:black; }
+</style>
+  </head>
+
+<body dir="rtl">
+<div class="container">
+<h1>سوق الأنتخابات</h1>
+<a href="./buy/" class="btn btn-large btn-primary">اشري صوت<br /><sub>2 دينار لايت</sub></a>
+<a href="./sell/" class="btn btn-large btn-danger">بيع صوتك<br /><sub>1 دينار لايت</sub></a>
+</div>
+</body>
+</html>
---
+++ sell/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>VOX - بيع صوتك</title>
+  <link href="http://getbootstrap.com/2.3.2/assets/css/bootstrap.css" rel="stylesheet">
+<style>
+@font-face {
+  font-family:'Scheherazade';
+  src:url('http://fkr.pirate.tn/ScheherazadeRegOT.ttf');
+}
+@font-face {
+  font-family:'Droid';
+  src:url('http://fkr.pirate.tn/DroidKufi-Regular.ttf');
+}
+body { font-family: Droid; text-align: center}
+.btn {font-family: Droid;}
+.footer { color:#fff100; background-color:black; }
+</style>
+  </head>
+
+<body dir="rtl">
+<div class="container">
+<h1>بيع صوتك</h1>
+<form method="post" action=".">
+<label>أسم</label>
+<input type="text" />
+<label>لقب</label>
+<input type="text" />
+<label>رقم الهاتف الجوال</label>
+<input type="text" />
+<br />
+<button class="btn btn-large">بيع</button>
+</form>
+<p class="hero-unit">
+سجل معطياتك، بش نعرضو صوتك للبيع. الشاري بش يكلمك يقول لك لشكون تعطي صوتك و يبعثلك دينار لايت. إكتب اسمك صحيح، اللقب مش إجباري.
+</p>
+</div>
+</body>
+</html>
