f4addfaa15e51f6c8eb2719db13cd3ab90de7f0f
Slim Amamou <slim.amamou@gmail.com>
2014-09-23 16:28:26 +0100

    database

--- sell/index.php
+++ sell/index.php
@@ -1,13 +1,20 @@
+<?php
+try {
+		$q = $db->prepare('INSERT into contacts (id, phone, name) values (:id, :phone, :name)');
+		$q->bindValue(':id', hash('sha256', $_POST['phone']));
+		$q->bindValue(':phone', $_POST['phone']);
+		$q->bindValue(':name', $_POST['name']);
+		$q->execute();
+	} catch (Exception $e) {
+		die($e->getMessage());
+	}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <title>VOX - بيع صوتك</title>
   <link href="http://getbootstrap.com/2.3.2/assets/css/bootstrap.css" rel="stylesheet">
 <style>
-@font-face {
-  font-family:'Scheherazade';
-  src:url('http://fkr.pirate.tn/ScheherazadeRegOT.ttf');
-}
 @font-face {
   font-family:'Droid';
   src:url('http://fkr.pirate.tn/DroidKufi-Regular.ttf');
