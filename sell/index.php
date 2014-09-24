066e933171a3eddb08be1f12e4c4c3ce89d59ae2
Slim Amamou <slim.amamou@gmail.com>
2014-09-24 17:21:04 +0100

    bugfix

--- sell/index.php
+++ sell/index.php
@@ -1,13 +1,17 @@
 <?php
-try {
+	require "../ini.php";
+
+if ($_POST["phone"]) {
+	try {
 		$q = $db->prepare('INSERT into contacts (id, phone, name) values (:id, :phone, :name)');
 		$q->bindValue(':id', hash('sha256', $_POST['phone']));
 		$q->bindValue(':phone', $_POST['phone']);
 		$q->bindValue(':name', $_POST['name']);
 		$q->execute();
 	} catch (Exception $e) {
-		die($e->getMessage());
+		fatal_error($e->getMessage());
 	}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,13 +34,14 @@ body { font-family: Droid; text-align: center}
 <h1>بيع صوتك</h1>
 <form method="post" action=".">
 <label>أسم</label>
-<input type="text" />
+<input type="text" name="name" />
 <label>لقب</label>
-<input type="text" />
+<input type="text" name="family" />
 <label>رقم الهاتف الجوال</label>
-<input type="text" />
+<input type="text" name="phone" />
 <br />
-<button class="btn btn-large">بيع</button>
+<button class="btn btn-large btn-success">بيع</button>
+<a href=".." class="btn">بطلت، ما عادش بائع </a>
 </form>
 <p class="hero-unit">
 سجل معطياتك، بش نعرضو صوتك للبيع. الشاري بش يكلمك يقول لك لشكون تعطي صوتك و يبعثلك دينار لايت. إكتب اسمك صحيح، اللقب مش إجباري.
